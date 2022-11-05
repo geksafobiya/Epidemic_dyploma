a0c07409fd41cad180d07a3c7aa6207a59d9a9ac
geksafobiya <asja653@gmail.com>
2022-11-06 01:44:06 +0200

    working file 1.0 працюють коефіцієнти (перші два)

---
+++ epidemics_plotter.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+
+# Python standard library modules
+import sys
+
+# 3rd party modules
+import matplotlib
+import matplotlib.backends.backend_qt5agg as backend_qt5agg
+from matplotlib.figure import Figure
+import PyQt5.QtCore as QtCore
+import PyQt5.QtGui as QtGui
+import PyQt5.QtWidgets as QtWidgets
+
+# Local application modules
+from growth_calculator import GrowthCalculator
+from options_menu import OptionsMenu_SIR
+#from options_menu_4_species import OptionsMenu_4_species
+#from options_menu_2 import OptionsMenu_2_species
+
+#import resources
+
+APP_NAME = 'Lotka-Volterra'
+AUTHOR = 'Клименко Анастасiя'
+
+class AppForm_3_species(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+
+        # название окна
+        self.setWindowTitle(APP_NAME)
+
+        # Создание в меню параметров в виджете док-станции
+        self.options_menu = OptionsMenu_SIR()
+        dock = QtWidgets.QDockWidget('Налаштування коефіцієнтів', self)
+        dock.setFeatures(
+            QtWidgets.QDockWidget.NoDockWidgetFeatures |
+            QtWidgets.QDockWidget.DockWidgetMovable |
+            QtWidgets.QDockWidget.DockWidgetFloatable
+        )
+        dock.setAllowedAreas(
+            QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea,
+        )
+        dock.setWidget(self.options_menu)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
+
+        # Подключение сигналов из меню параметров
+        self.options_menu.update_btn.clicked.connect(self.clear_graph)
+        self.options_menu.update_btn.clicked.connect(self.calculate_data)
+
+        self.options_menu.clear_graph_btn.clicked.connect(self.clear_graph)
+        self.options_menu.legend_cb.stateChanged.connect(self.redraw_graph)
+        self.options_menu.grid_cb.stateChanged.connect(self.redraw_graph)
+        self.options_menu.legend_loc_cb.currentIndexChanged.connect(self.redraw_graph)
+
+        # создание графика
+        fig = Figure((7.0, 3.0), dpi=100)
+        self.canvas = backend_qt5agg.FigureCanvasQTAgg(fig)
+        self.canvas.setParent(self)
+        self.axes = fig.add_subplot(111)
+        backend_qt5agg.NavigationToolbar2QT(self.canvas, self.canvas)
+
+        # инициализация графа
+        self.clear_graph()
+
+        self.setCentralWidget(self.canvas)
+
+        # создать менюбар действий
+
+        about_action = QtWidgets.QAction('&About', self)
+        about_action.setToolTip('About')
+        about_action.setIcon(QtGui.QIcon(':/resources/icon_info.png'))
+        about_action.triggered.connect(self.show_about)
+
+     # создать менюбар
+        file_exit_action = QtWidgets.QAction('&Exit', self)
+        file_exit_action.setToolTip('Exit')
+        file_exit_action.setIcon(QtGui.QIcon(':/resources/door_open.png'))
+        file_exit_action.triggered.connect(self.close)
+
+        file_menu = self.menuBar().addMenu('&Exit')
+        file_menu.addAction(file_exit_action)
+
+        help_menu = self.menuBar().addMenu('&Help')
+        help_menu.addAction(about_action)
+
+    def calculate_data(self):
+        # объект GrowthCalculator
+        growth = GrowthCalculator()
+
+        # Update the GrowthCalculator parameters from the GUI options
+     #   growth.a11 = self.options_menu.a11_sb.value()
+        growth.gamma = self.options_menu.gamma_sb.value()
+        growth.beta = self.options_menu.beta_sb.value()
+
+        growth.sus = self.options_menu.sus_sb.value()
+        growth.inf = self.options_menu.inf_sb.value()
+        growth.rec = self.options_menu.rec_sb.value()
+
+        growth.iterations = self.options_menu.iterations_sb.value()
+        growth.dt = self.options_menu.timedelta_sb.value()
+
+        # Calculate the population growths
+        results = growth.calculate()
+        self.infectious_history.extend(results['Infectious'])
+        self.susceptible_history.extend(results['Susceptible'])
+        self.recovered_history.extend(results['Recovered'])
+
+        if (len(self.infectious_history) == 0 and
+                len(self.susceptible_history) == 0 and
+                len(self.recovered_history) == 0):
+            QtWidgets.QMessageBox.information(self, 'Error', 'Помилка')
+            return
+
+        # последнее в векторе количество популяции на панель инструментов параметров
+      #  print('self.predator_history[-1]', self.predator_history[-1])
+      #  self.options_menu.predator_sb.setValue(self.predator_history[-1])
+      #  self.options_menu.prey_sb.setValue(self.prey_history[-1])
+      #  self.options_menu.superpredators_sb.setValue(self.superpredator_history[-1])
+        # перерисовать граф
+        self.redraw_graph()
+
+    def clear_graph(self):
+        # очистить историю популяций
+        self.infectious_history = []
+        self.susceptible_history = []
+        self.recovered_history = []
+
+        # перерисовать граф
+        self.redraw_graph()
+
+    def redraw_graph(self):
+        # очистить граф
+        self.axes.clear()
+
+        # Create the graph labels
+        self.axes.set_title('Цикл роста хищников и травоядных вида 1 и вида 2')
+        self.axes.set_xlabel('Итерации')
+        self.axes.set_ylabel('Размер популяции')
+
+        # Plot the current population data
+        if self.recovered_history:
+            self.axes.plot(self.recovered_history, 'r-', label='очухалися')
+        if self.susceptible_history:
+            self.axes.plot(self.susceptible_history, 'b-', label='скоро здохнуть')
+        if self.infectious_history:
+            self.axes.plot(self.infectious_history, 'g-', label='хворі на голову')
+
+        # если нужно, создаём легенду
+        if self.options_menu.legend_cb.isChecked():
+            if self.recovered_history or self.susceptible_history or self.infectious_history:
+                legend_loc = str(
+                    self.options_menu.legend_loc_cb.currentText()
+                ).lower()
+                legend = matplotlib.font_manager.FontProperties(size=10)
+                self.axes.legend(loc=legend_loc, prop=legend)
+
+        # если нужно, сетки на графике
+        self.axes.grid(self.options_menu.grid_cb.isChecked())
+
+        # рисуем график
+        self.canvas.draw()
+
+    def show_about(self):
+
+        message = '''<font size="+2">%s</font>
+            <p>Дипломна робота на тему "моделювання епідеміологічних моделей".
+            <p>Написана %s, група КА-13мп.
+            ''' %(APP_NAME, AUTHOR)
+
+        QtWidgets.QMessageBox.about(self, 'About' + APP_NAME, message)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon(':/resources/icon.svg'))
+    form = AppForm_3_species()
+    form.show()
+    app.exec_()
---
+++ options_menu.py
@@ -0,0 +1,166 @@
+# 3rd party modules
+import PyQt5.QtCore as QtCore
+import PyQt5.QtGui as QtGui
+import PyQt5.QtWidgets as QtWidgets
+
+
+class OptionsMenu_SIR(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        # Create the "SIR Coefficients" options
+        self.beta_sb = QtWidgets.QDoubleSpinBox()
+        self.gamma_sb = QtWidgets.QDoubleSpinBox()
+        self.t_recovery = QtWidgets.QDoubleSpinBox()
+        self.R0 = QtWidgets.QDoubleSpinBox()
+
+
+
+        for widget in (self.beta_sb, self.gamma_sb, self.t_recovery, self.R0):
+            widget.setRange(0, 10)
+            widget.setSingleStep(0.01)
+
+        coeff_grid = QtWidgets.QGridLayout()
+        coeff_grid.addWidget(QtWidgets.QLabel('Константа швидкості'), 0, 0)
+        coeff_grid.addWidget(self.beta_sb, 0, 1)
+
+        coeff_grid.addWidget(QtWidgets.QLabel('Швидкість одужання'), 1, 0)
+        coeff_grid.addWidget(self.gamma_sb, 1, 1)
+        coeff_grid.addWidget(QtWidgets.QLabel('середній час одужання після інфекції'), 2, 0)
+        coeff_grid.addWidget(self.t_recovery, 2, 1)
+
+        coeff_grid.addWidget(QtWidgets.QLabel('Передаваність хвороби'), 3, 0)
+        coeff_grid.addWidget(self.R0, 3, 1)
+
+        coeff_gb = QtWidgets.QGroupBox('Коефіцієнти SIR-моделі:')
+        coeff_gb.setLayout(coeff_grid)
+
+        # Create the "Other Parameters" options
+        self.sus_sb = QtWidgets.QDoubleSpinBox()
+        self.sus_sb.setRange(0, 100000)
+        self.sus_sb.setSingleStep(1)
+
+        self.inf_sb = QtWidgets.QDoubleSpinBox()
+        self.inf_sb.setRange(0, 100000)
+        self.inf_sb.setSingleStep(1)
+
+        self.rec_sb = QtWidgets.QDoubleSpinBox()
+        self.rec_sb.setRange(0, 100000)
+        self.rec_sb.setSingleStep(1)
+
+        self.iterations_sb = QtWidgets.QSpinBox()
+        self.iterations_sb.setRange(0, 100000)
+        self.iterations_sb.setSingleStep(100)
+
+        self.timedelta_sb = QtWidgets.QDoubleSpinBox()
+        self.timedelta_sb.setRange(0, 100)
+        self.timedelta_sb.setSingleStep(0.05)
+
+        other_grid = QtWidgets.QGridLayout()
+        other_grid.addWidget(QtWidgets.QLabel('Сприятливі:'), 0, 0)
+        other_grid.addWidget(self.sus_sb, 0, 1)
+        other_grid.addWidget(QtWidgets.QLabel('Інфіковані'), 1, 0)
+        other_grid.addWidget(self.inf_sb, 1, 1)
+        other_grid.addWidget(QtWidgets.QLabel('очухані:'), 2, 0)
+        other_grid.addWidget(self.rec_sb, 2, 1)
+
+        other_grid.addWidget(QtWidgets.QLabel('Ітерації'), 3, 0)
+        other_grid.addWidget(self.iterations_sb, 3, 1)
+        other_grid.addWidget(QtWidgets.QLabel('Час дельта'), 4, 0)
+        other_grid.addWidget(self.timedelta_sb, 4, 1)
+
+        other_gb = QtWidgets.QGroupBox('Інші параметри:')
+        other_gb.setLayout(other_grid)
+
+        # Create the "Graph Options" options
+        self.legend_cb = QtWidgets.QCheckBox('Показати легенду')
+        self.legend_cb.setChecked(True)
+        self.legend_cb.stateChanged.connect(self.legend_change)
+
+        self.grid_cb = QtWidgets.QCheckBox('Показати сітку')
+        self.grid_cb.setChecked(True)
+        self.legend_loc_lbl = QtWidgets.QLabel('Місцезнаходження легенди')
+        self.legend_loc_cb = QtWidgets.QComboBox()
+        self.legend_loc_cb.addItems([x.title() for x in [
+            'right',
+            'center',
+            'lower left',
+            'center right',
+            'upper left',
+            'center left',
+            'upper right',
+            'lower right',
+            'upper center',
+            'lower center',
+            'best',
+        ]])
+        self.legend_loc_cb.setCurrentIndex(6)
+
+        cb_box = QtWidgets.QHBoxLayout()
+        cb_box.addWidget(self.legend_cb)
+        cb_box.addWidget(self.grid_cb)
+
+        legend_box = QtWidgets.QHBoxLayout()
+        legend_box.addWidget(self.legend_loc_cb)
+        legend_box.addStretch()
+
+        graph_box = QtWidgets.QVBoxLayout()
+        graph_box.addLayout(cb_box)
+        graph_box.addWidget(self.legend_loc_lbl)
+        graph_box.addLayout(legend_box)
+
+        graph_gb = QtWidgets.QGroupBox('Налаштування графіку:')
+        graph_gb.setLayout(graph_box)
+
+        # Create the update/reset buttons
+        self.update_btn = QtWidgets.QPushButton(
+            QtGui.QIcon(':/resources/calculator.png'),
+            'Запуск ітерацій')
+
+       # self.inf_btn = QtWidgets.QPushButton('Інфіковані')
+       # self.preyPredator_btn = QtWidgets.QPushButton('Хищники-жертвы')
+
+        self.reset_values_btn = QtWidgets.QPushButton(
+            QtGui.QIcon(':/resources/arrow_undo.png'),
+            'Сброс значений')
+        self.clear_graph_btn = QtWidgets.QPushButton(
+            QtGui.QIcon(':/resources/chart_line_delete.png'),
+            'Очистить график')
+
+        self.reset_values_btn.clicked.connect(self.reset_values)
+
+     #   self.preySuperpredator_btn.clicked.connect(self.preySuperpredator)
+      #  self.preyPredator_btn.clicked.connect(self.preyPredator)
+
+        # Create the main layout
+        container = QtWidgets.QVBoxLayout()
+        container.addWidget(coeff_gb)
+        container.addWidget(other_gb)
+        container.addWidget(graph_gb)
+        container.addWidget(self.update_btn)
+        container.addStretch()
+     #   container.addWidget(self.preySuperpredator_btn)
+     #   container.addWidget(self.preyPredator_btn)
+     #   container.addStretch()
+        container.addWidget(self.reset_values_btn)
+        container.addWidget(self.clear_graph_btn)
+        self.setLayout(container)
+
+        # Populate the widgets with values
+        self.reset_values()
+
+
+    def reset_values(self):
+
+        self.beta_sb.setValue(0.1)
+        self.gamma_sb.setValue(0.1)
+
+        self.sus_sb.setValue(5)
+        self.inf_sb.setValue(20)
+        self.rec_sb.setValue(5)
+        self.iterations_sb.setValue(1000)
+        self.timedelta_sb.setValue(0.02)
+
+    def legend_change(self):
+        self.legend_loc_cb.setEnabled(self.legend_cb.isChecked())
+        self.legend_loc_lbl.setEnabled(self.legend_cb.isChecked())
